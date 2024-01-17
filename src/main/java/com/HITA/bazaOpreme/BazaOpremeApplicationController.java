86c81080adc8147400e1fbf85e6c903320c2953b
TomislavMisak <tomislav.misak@hotmail.com>
2024-01-17 21:07:49 +0100

    Isprobano radi li spring sa početnom stranicom

---
+++ src/main/java/com/HITA/bazaOpreme/BazaOpremeApplicationController.java
@@ -0,0 +1,27 @@
+package com.HITA.bazaOpreme;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+    public class BazaOpremeApplicationController {
+
+
+@GetMapping("/pocetna")
+    public String pocetna() {
+        return "pocetna.html";
+    }
+}
