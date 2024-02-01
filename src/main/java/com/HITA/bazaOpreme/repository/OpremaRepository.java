880ee54d12fa6ee2a3b3a72d8ceb1a6aedf5dbb4
TomislavMisak <tomislav.misak@hotmail.com>
2024-02-01 10:02:20 +0100

    Pocetna.html rješena

--- src/main/java/com/HITA/bazaOpreme/repository/OpremaRepository.java
+++ src/main/java/com/HITA/bazaOpreme/repository/OpremaRepository.java
@@ -1,11 +1,18 @@
 package com.HITA.bazaOpreme.repository;
 
 import com.HITA.bazaOpreme.model.Oprema;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OpremaRepository extends JpaRepository<Oprema, Long> {
     // Custom query methods can be added here if needed
+
+
+    @Override
+    List<Oprema> findAll();
 }
 
