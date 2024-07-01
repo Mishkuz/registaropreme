1aa6aac5c60a3cff2428a3b78879956d5203f28b
lovrostuzic <goldstudiostu@gmail.com>
2024-07-02 00:10:02 +0200

    Seriazable User

--- src/main/java/com/HITA/bazaOpreme/repository/KorisnikRepository.java
+++ src/main/java/com/HITA/bazaOpreme/repository/KorisnikRepository.java
@@ -5,8 +5,9 @@ import com.HITA.bazaOpreme.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
+@Repository
 public interface KorisnikRepository extends JpaRepository<Korisnik, Long> {
     Korisnik findByEmailAndPassword(String email, String password);
 
